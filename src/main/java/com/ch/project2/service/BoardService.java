5f51e0a24d8857abae4e1beb227c8556dc5c2a29
Jisun Oh <87827545+didoniii@users.noreply.github.com>
2022-02-09 02:54:06 +0900

    2/8

--- src/main/java/com/ch/project2/service/BoardService.java
+++ src/main/java/com/ch/project2/service/BoardService.java
@@ -8,24 +8,24 @@ import com.ch.project2.model.Category;
 
 public interface BoardService {
 	
-	List<Category> getCategories();
+	List<Category> getCategories();							// 카테고리 리스트
 
-	int getBoardCount();
-
-	int insertBoard(Board board);
-
-	Board getBoard(int b_no);
+	int getBoardCount();									// 조회수 ( 우선 만들어 놓음 )
 	
-	List<Board> searchBoard(Map<String, Object> param);
+	int getMaxB_no();										// 최신 게시글 번호 
 
-	int getSearchBoardCount(Map<String, Object> param);
+	int insertBoard(Board board);							// 게시글 DB에 넣기 (게시글 작성)
 
-	int updateBoard(Board board);
+	Board getBoard(int b_no);								// 게시글 조회
+	
+	int updateBoard(Board board);							// 게시글 수정
+	
+	int getSearchBoardCount(Map<String, Object> param);		// 검색된 게시글 수
+	
+	List<Board> searchBoard(Map<String, Object> param);		// 검색된 게시글 리스트 (한 페이지 당)
 
-	int getMaxB_no();
+	int selectTotalMyBoard(String m_id);					// 내가 작성한 게시글 수
 
-	int selectTotalMyBoard(String m_id);
-
-	List<Board> selectMyBoard(Map<String, Object> param);
+	List<Board> selectMyBoard(Map<String, Object> param);	// 내가 작성한 게시글 리스트 (한 페이지 당)
 }
 
