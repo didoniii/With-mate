4e11a256c1d6f2d8c38938677ecd2a20d0fe0e5c
Jisun Oh <87827545+didoniii@users.noreply.github.com>
2022-02-11 00:00:23 +0900

    2/10

--- src/main/java/com/ch/project2/service/MemberService.java
+++ src/main/java/com/ch/project2/service/MemberService.java
@@ -1,5 +1,15 @@
 package com.ch.project2.service;
 
-public interface MemberService {
+import com.ch.project.model.Member;
 
+public interface MemberService {
+	Member selectMember(String m_id);
+	Member selectMemberWithNick(String nickname);
+	int insert(Member member);
+	int updateProfile(Member member);
+	
+	//멤버 정보 수정
+	int updateMember(Member member);
+	
+	int updateRating(Member member);
 }
