7bd5e6d82f1f3db97505c829098e39db5887c5ed
Jisun Oh <87827545+didoniii@users.noreply.github.com>
2022-02-12 03:20:26 +0900

    2/11

--- src/main/java/com/ch/project2/service/MemberService.java
+++ src/main/java/com/ch/project2/service/MemberService.java
@@ -1,15 +1,18 @@
 package com.ch.project2.service;
 
-import com.ch.project.model.Member;
+import com.ch.project2.model.Member;
 
 public interface MemberService {
-	Member selectMember(String m_id);
-	Member selectMemberWithNick(String nickname);
-	int insert(Member member);
-	int updateProfile(Member member);
+	Member selectMember(String m_id);				// 회원 정보 조회
+	
+	Member selectMemberWithNick(String nickname);	// 닉네임 중복체크 
+	
+	int insert(Member member);						// 회원 가입
+	
+	int updateProfile(Member member);				// 프로필 정보 입력
 	
 	//멤버 정보 수정
-	int updateMember(Member member);
-	
-	int updateRating(Member member);
+	int updateMember(Member member);				//
+			
+	int updateRating(Member member);				//
 }
--- src/main/java/com/ch/project2/service/MemberServiceImpl.java
+++ src/main/java/com/ch/project2/service/MemberServiceImpl.java
@@ -1,7 +1,31 @@
 package com.ch.project2.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.ch.project2.dao.MemberDao;
+import com.ch.project2.model.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService {
+	@Autowired
+	private MemberDao md;
+	
+	public Member selectMember(String m_id) {
+		return md.selectMember(m_id);
+	}
+	public Member selectMemberWithNick(String nickname) {
+		return md.selectMemberWithNick(nickname);
+	}
+	public int insert(Member member) {
+		return md.insert(member);
+	}
+	public int updateProfile(Member member) {
+		return md.updateProfile(member);
+	}
+	public int updateMember(Member member) {
+		return md.updateMember(member);
+	}
+	public int updateRating(Member member) {
+		return md.updateRating(member);
+	}
 }
